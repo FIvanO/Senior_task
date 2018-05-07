b20037151a2043f7560f46fbd6c3bf170ef8b750
FIvanO <FIvanO1998@gmail.com>
2018-05-07 16:51:24 +0300

    5 exercise

--- AyxCppTest/exercise5.cpp
+++ AyxCppTest/exercise5.cpp
@@ -1,5 +1,7 @@
 #include "catch/catch.hpp" 
 #include "Add.h"
+#include <string>
+#include <iostream>
 
 namespace AyxCppTest
 {
@@ -16,22 +18,65 @@ namespace AyxCppTest
 	*/
 	std::string Add(std::string lhs, std::string rhs)
 	{
-		lhs;
-		rhs;
-		std::string retval;
+		bool overfl = false;
+		int a[1000];
+		int b[1000];
+		int ls = lhs.length();
+		int rs = rhs.length();
+		if (ls < rs) {
+			std::string ss = lhs;
+			lhs = rhs;
+			rhs = ss;
+			int lss = ls;
+			ls = rs;
+			rs = lss;
+		}
+		int j = 0;
+		for (int i = ls - 1; i >= 0; --i) {
+			a[j++] = int(lhs[i] - '0');
+		}
+		j = 0;
+		for (int i = rs - 1; i >= 0; --i) {
+			b[j++] = int(rhs[i] - '0');
+		}
+		std::string retval = "";
+		for (int i = 0; i < rs; ++i) {
+			int c = a[i] + b[i];
+			if (overfl) {
+				c++;
+			}
+			retval = std::to_string(c % 10) + retval;
+			overfl = c > 9;
+		}
+		for (int i = rs; i < ls; ++i) {
+			int c = a[i];
+			if (overfl) {
+				c++;
+			}
+			retval = std::to_string(c % 10) + retval;
+			overfl = c > 9;
+		}
+		if (overfl) {
+			retval = "1" + retval;
+		}
 		return retval;
 	}
 
 	TEST_CASE("exercise5")
 	{
 		// enable to run exercise 6 tests
-#if 0
+#if 1
 		REQUIRE(Add("1", "2") == "3");
 
 		std::string                       BigNum("1000000000000000000000000000000000000000000000000000000000000");
-		REQUIRE(Add(std::move(BigNum), "1")   == "1000000000000000000000000000000000000000000000000000000000001");
-		REQUIRE(Add(std::move(BigNum), "10")  == "1000000000000000000000000000000000000000000000000000000000010");
-		REQUIRE(Add(std::move(BigNum), "100") == "1000000000000000000000000000000000000000000000000000000000100");
+		REQUIRE(Add(BigNum, "1")   == "1000000000000000000000000000000000000000000000000000000000001");
+		REQUIRE(Add(BigNum, "10")  == "1000000000000000000000000000000000000000000000000000000000010");
+		REQUIRE(Add(BigNum, "100") == "1000000000000000000000000000000000000000000000000000000000100");
+		REQUIRE(Add("12345678999999", "123456789") == "12345802456788"); // new test case
+		REQUIRE(Add("99999999999999999999", "99999999999999999999") == "199999999999999999998"); // new test case
+		REQUIRE(Add("0", "99999") == "99999"); // new test case
+		REQUIRE(Add("1", "99999") == "100000"); // new test case
+		REQUIRE(Add("11", "99999") == "100010"); // new test case
 #endif
 	}
 }
